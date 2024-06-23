9d39abb1794da25de497d01065da89e19d4476b1
Vasanthan <srvasanthan33@outlook.com>
2024-06-24 05:06:32 +0530

    image retrived by id , - added

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,17 +6,39 @@ const fs = require('fs');
 
 const getThumbnailById = async (req, res) => {
 
-}
+    try {
+        const movieId = req.params.id;
+        const movieMedia = await mediaModel.findOne({ movieId }).lean()
 
-const getAllMovies = async (req, res) => {
+        if (!movieMedia || !movieMedia.image || !movieMedia.image.filePath) {
+            return res.status(404).json({ message: 'Image not found for this movie' });
+        }
 
-}
+        const imagePath = path.resolve(movieMedia.image.filePath);
 
-const getMovieById = async (req, res) => {
+        if (fs.existsSync(imagePath)) {
 
+            const thumbnailURL = `http://localhost:5500/api/v1/flickk/thumbnail/${movieId}.jpg`;
+            res.json({ thumbnailURL }); // Respond with the URL
+        } else {
+            res.status(404).json({ message: 'Image file not found on server' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 const getVideoById = async (req, res) => {
-
+    res.send('video by id')
 }
+
+const getAllMovies = async (req, res) => {
+    res.send('all movies')
+}
+
+const getMovieById = async (req, res) => {
+    res.send('movie by id')
+}
+
+
 module.exports = { getThumbnailById, getVideoById, getMovieById, getAllMovies }
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 
-app.use('/api/vi/flickk', homeRoute)
+app.use('/api/v1/flickk', homeRoute)
 
 // Protected routes can be accessed only when authorized
 app.use('/api/v1/movies', requireAuth, checkUser, movieRoute)
