ce909126c33d329f9fdb2b3a4d227e7523d95d1c
Vasanthan <srvasanthan33@outlook.com>
2024-06-24 10:49:55 +0530

    implemented cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 require('dotenv').config()
 const path = require('path')
 
@@ -25,10 +26,13 @@ mongoose.connect(process.env.MONGO_URL)
     })
 
 
-
+app.get('/', (req, res) => {
+    res.send('Welcome to flickk , a Movie Rating App')
+})
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 
 app.use('/api/v1/flickk', homeRoute)
